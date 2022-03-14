646ea6352926c3296b9f37389c61a8d7b4124418
noahlopcor <99145902+noahlopcor@users.noreply.github.com>
2022-03-14 11:57:04 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,16 @@
+function girar90gradosalaizquierda () {
+    maqueen.motorStop(maqueen.Motors.All)
+    basic.pause(200)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 20)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 20)
+    basic.pause(1050)
+    maqueen.motorStop(maqueen.Motors.All)
+}
+basic.forever(function () {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 4) {
+        maqueen.motorStop(maqueen.Motors.All)
+        girar90gradosalaizquierda()
+    } else {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
+    }
+})
