0fa5efa9f48118d19c9088b0e297d55b09e1c366
noahlopcor <99145902+noahlopcor@users.noreply.github.com>
2022-03-16 09:14:13 +0100

    hola

--- main.ts
+++ main.ts
@@ -6,10 +6,37 @@ function girar90gradosalaizquierda () {
     basic.pause(1100)
     maqueen.motorStop(maqueen.Motors.All)
 }
+function girar90gradosaladerecha () {
+    maqueen.motorStop(maqueen.Motors.All)
+    basic.pause(200)
+    maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 20)
+    maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 20)
+    basic.pause(1100)
+    maqueen.motorStop(maqueen.Motors.All)
+}
+let ultimogiro = 0
+basic.forever(function () {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 4) {
+        maqueen.motorStop(maqueen.Motors.All)
+        if (ultimogiro == 0) {
+            girar90gradosalaizquierda()
+            ultimogiro = 1
+        } else {
+            girar90gradosaladerecha()
+            ultimogiro = 0
+        }
+    } else {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
+    }
+})
 basic.forever(function () {
     if (maqueen.Ultrasonic(PingUnit.Centimeters) < 4) {
         maqueen.motorStop(maqueen.Motors.All)
         girar90gradosalaizquierda()
+        if (maqueen.Ultrasonic(PingUnit.Centimeters) < 4) {
+            maqueen.motorStop(maqueen.Motors.All)
+            girar90gradosaladerecha()
+        }
     } else {
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 20)
     }
